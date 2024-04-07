1f324c7c676394e1b94cff683b3c3d78db629914
revenzeninb <166286947+revenzeninb@users.noreply.github.com>
2024-04-07 16:38:53 +0000

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,7 +138,7 @@ contextInfo: {
 externalAdReply: {
 title: 'BOBIZA BOT ♥',
 body: "أول بوت واتساب في العالم العربي 💖",
-thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
+thumbnailUrl: 'https://telegra.ph/file/4f77484c5b91567e2b1fa.jpg',
 sourceUrl: 'https://instagram.com/noureddine_ouafy',
 mediaType: 1,
 renderLargerThumbnail: true
